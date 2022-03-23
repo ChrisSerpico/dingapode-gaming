4ebfbbf6f2163faf737dee129d33183a4b118ea8
Chris Serpico <christopherserpico@gmail.com>
2022-03-23 18:08:20 -0500

    Set repo up for firebase deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDXK8O2j0UI8Igow9xiCvZr40WOSMVcwgE',
+    authDomain: 'dingapode-gaming.firebaseapp.com',
+    projectId: 'dingapode-gaming',
+    storageBucket: 'dingapode-gaming.appspot.com',
+    messagingSenderId: '878304973092',
+    appId: '1:878304973092:web:8d41bbe7620193ad0463b1',
+  },
 };
